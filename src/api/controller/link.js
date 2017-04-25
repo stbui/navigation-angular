de91cb13eb9b8c46d61031f9de95af420bb35817
stbui <stbui@stbui.com>
2017-04-25 16:38:20 +0800

    支持cors

--- src/api/controller/link.js
+++ src/api/controller/link.js
@@ -32,6 +32,7 @@ export default class extends Base {
     }
 
     setCorsHeader(){
+        this.header('Access-Control-Allow-Origin', this.header('origin') || '*');
         this.header('Access-Control-Allow-Headers', 'x-requested-with');
         this.header('Access-Control-Request-Method', 'GET,POST,PUT,DELETE');
         this.header('Access-Control-Allow-Credentials', 'true');
